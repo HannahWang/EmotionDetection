a77982862e377ad9e3afff4be3b6c2cddd0c6219
HannahWang <b00901023@ntu.edu.tw>
2015-06-03 09:49:51 +0800

    add eyes and mouth detection

--- face_vedio.py
+++ face_vedio.py
@@ -3,8 +3,12 @@ import sys
 import os
 
 #cascPath = sys.argv[1]
-cascPath = 'data/haarcascades/haarcascade_frontalface_default.xml'
-faceCascade = cv2.CascadeClassifier(cascPath)
+cascPath_face = 'data/haarcascades/haarcascade_frontalface_default.xml'
+cascPath_eye = 'data/haarcascades/haarcascade_eye.xml'
+cascPath_mouth = 'data/haarcascades/haarcascade_mcs_mouth.xml'
+faceCascade = cv2.CascadeClassifier(cascPath_face)
+eyeCascade = cv2.CascadeClassifier(cascPath_eye)
+mouthCascade = cv2.CascadeClassifier(cascPath_mouth)
 
 video_capture = cv2.VideoCapture(0)
 
@@ -18,13 +22,34 @@ while True:
 		gray,
 		scaleFactor=2,
 		minNeighbors=5,
-		minSize=(25, 25),
+		minSize=(30, 30),
 		flags=cv2.cv.CV_HAAR_SCALE_IMAGE
 	)
-
 	# Draw a rectangle around the faces
 	for (x, y, w, h) in faces:
-		cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		face_gray = gray[y:y+h, x:x+w]
+		face_color = frame[y:y+h, x:x+w]
+		eyes = eyeCascade.detectMultiScale(
+			face_gray,
+			scaleFactor = 2,
+			minNeighbors = 5,
+			minSize = (10,10),
+		)
+		print "eyes:\n"
+		print eyes
+		for (ex,ey,ew,eh) in eyes:
+			cv2.rectangle(face_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
+		mouth = mouthCascade.detectMultiScale(
+			face_gray,
+			scaleFactor = 6.5,
+			minNeighbors = 5,
+			minSize = (10,10),
+		)
+		print "mouth:\n" 
+		print mouth
+		for (mx, my, mw, mh) in mouth:
+			cv2.rectangle(face_color,(mx,my),(mx+mw,my+mh), (255,255,255),2)
 
 	# Display the resulting frame
 	cv2.imshow('Video', frame)
